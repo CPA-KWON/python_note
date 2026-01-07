993b2e83036c846411198981f52ea0ce6a33c07d
Lee Su Min <rnjstpdnjs34@gmail.com>
2026-01-07 21:08:11 +0900

    day 1

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def plus(a,b):
+    c=a+b
+    print(a,"+",b,"는",c,"입니다")
+def minus(e,f):
+    g=e-f
+    print(e,"-",f,"는",g,"입니다")
+def div(h,i):
+    j=h/i
+    print(h,"/",i,"는",j,"입니다")
+div(17,3)
